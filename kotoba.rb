0935fc51afb4778db17d172516417697ca860fcd
Paul Traylor <paul@wegame.com>
2010-12-29 00:16:19 -0800

    Initial version of simple Anki import conversion script

---
+++ kotoba.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+raise 'Missing Input File' if not ARGV[0]
+
+def format( words)
+	if words[0] == words[1] then
+		puts "#{words[0]}\t#{words[2]}\t#{words[1]}"
+	else
+		# We use <Word>[<Reading>] at the end to automatically add furigana
+		puts "#{words[0]}\t#{words[2]}\t#{words[0]}[#{words[1]}]"
+	end
+end
+
+input = File.new(ARGV[0], "r")
+while(line = input.gets)
+	# Kotoba lines are of the form
+	# <DICT> <ID> <Word> <Reading> <Definition> <List>
+	# So we want to take <Word> <Reading> <Definition>
+	words = line.strip().split("\t").slice(2,3)
+	# Then we want to put it as <Word> <Meaning> <Reading> for Anki
+	format words
+end
+
+input.close
---
+++ rikaichan.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+raise 'Missing Input File' if not ARGV[0]
+
+def format words
+	if words[0] == words[1] then
+		# If there are only 2 'words' then we only have hiragana
+		puts "#{words[0]}\t#{words[2]}\t#{words[1]}"
+	else
+		# Otherwise we have kanji
+		# We use <Word>[<Reading>] at the end to automatically add furigana
+		puts "#{words[0]}\t#{words[2]}\t#{words[0]}[#{words[1]}]"
+	end
+end
+
+input = File.new(ARGV[0], "r")
+while(line = input.gets)
+	words = line.strip().split("\t")
+	if words.length == 2 then
+		# Use word[0] twice for both <Word> and <Reading>
+		words = [words[0],words[0],words[1]]
+	end
+	format words
+end
+
+input.close
