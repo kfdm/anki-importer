38b15ed5957f03895dcb57f496cffee135f9be2a
Paul Traylor <kungfudiscomonkey@gmail.com>
2011-09-13 19:47:15 -0700

    Remove old test file

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'Testing Rikaichan'
-echo 'tags: Import Rikaichan' > rikaichan.txt
-./rikaichan.rb ~/Dropbox/rikaichan.txt | tee -a rikaichan.txt
-
-echo ''
-echo 'Testing Kotoba'
-echo 'tags: Import Kotoba' > kotoba.txt
-./kotoba.rb ~/Dropbox/Favorites.txt | tee -a kotoba.txt
